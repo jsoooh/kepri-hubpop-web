28625a879390d64acd7287787cc662f3ec72b111
gbible45 <gbible45@gmail.com>
2018-12-01 13:49:35 +0900

    사용하지 않는 js 정리

--- iaas/js/stageLoad.js
+++ iaas/js/stageLoad.js
@@ -13,15 +13,15 @@ loadJavascript(_IAAS_STAGE_+'/js/controllers/compute/computeDetailControllers.js
 //loadJavascript(_IAAS_STAGE_+'/js/controllers/compute/deployServerConfigFormControllers.js'+_VERSION_TAIL_HEAD_);
 
 loadJavascript(_IAAS_STAGE_+'/js/controllers/compute/computeSnapshotControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/compute/computeInitScriptControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/securityPolicy/securityPolicyControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/securityPolicy/securityPolicyRuleControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/keypair/keypairControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/compute/computeInitScriptControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/securityPolicy/securityPolicyControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/securityPolicy/securityPolicyRuleControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/keypair/keypairControllers.js'+_VERSION_TAIL_HEAD_);
 
 loadJavascript(_IAAS_STAGE_+'/js/controllers/storage/storageControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/storage/objectStorageControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/storage/objectStorageControllers.js'+_VERSION_TAIL_HEAD_);
 loadJavascript(_IAAS_STAGE_+'/js/controllers/storage/storageSnapshotControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/network/networkControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/network/domainControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/network/domainRouterControllers.js'+_VERSION_TAIL_HEAD_);
-loadJavascript(_IAAS_STAGE_+'/js/controllers/network/networkExpansionControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/network/networkControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/network/domainControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/network/domainRouterControllers.js'+_VERSION_TAIL_HEAD_);
+//loadJavascript(_IAAS_STAGE_+'/js/controllers/network/networkExpansionControllers.js'+_VERSION_TAIL_HEAD_);
