4b7be34c361f519c6c718d6f117f352adff269d8
mhwalee <mhwalee@crossent.com>
2019-07-01 13:25:34 +0900

    2차 도메인 쿠키 공유를 위한 수정

--- comm/js/services/commonServices.js
+++ comm/js/services/commonServices.js
@@ -2963,8 +2963,10 @@ angular.module('common.services', ['LocalStorageModule'])
     .factory('cookies', function ($cookies) {
         var cookies = {};
         var cookiesOption = {path: _COOKIES_PATH_};
-        if (_DOMAIN_ && _DOMAIN_ == "www.kepri-demo.crossent.com") {
+        if (_DOMAIN_ == "www.kepri-demo.crossent.com") {
             var cookiesOption = {domain: _DOMAIN_.substring(3), path: _COOKIES_PATH_};
+        } else {
+            var cookiesOption = {domain: "." +_DOMAIN_, path: _COOKIES_PATH_};
         }
 
         cookies.getLanguageKey = function () {
