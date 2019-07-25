82fc505805102b6e9c7d0f4063673d13c56d57f4
yrimjang <yrimjang@crossent.com>
2019-07-25 13:32:27 +0900

    오브젝트 스토리지 화면 구현 (접속일,사용법 제외)

--- iaas/js/controllers/storage/objectStorageControllers.js
+++ iaas/js/controllers/storage/objectStorageControllers.js
@@ -21,6 +21,9 @@ angular.module('iaas.controllers')
             ct.fn.getObjectStorageList();
             ct.fn.getSendSecretInfoList();
         });
+        ct.presentDate = function(){
+            ct.toDay = moment(new Date()).format('YYYY-MM-DD');
+        }
 
         /*오브젝트 저장소 목록,정보,용량 조회*/
         ct.fn.getObjectStorageList = function(){
@@ -46,7 +49,7 @@ angular.module('iaas.controllers')
             $scope.main.loadingMainBody = true;
             var returnPromise = common.resourcePromise(CONSTANTS.iaasApiContextUrl + '/storage/objectStorage/sendSecretInfo', 'GET', {tenantId:ct.data.tenantId,containerName:ct.containerName}, 'application/x-www-form-urlencoded');
             returnPromise.success(function (data, status, headers) {
-                if(data.content) {
+                if (data.content) {
                     ct.sendSecretInfoList = data.content.secret
                 }
             });
@@ -83,65 +86,73 @@ angular.module('iaas.controllers')
                 });
             });
         };
+        ct.fn.createObjectStoragePop = function($event) {
 
-
-        /*[생성(+)] 클릭*/
-        ct.fn.createStorageObjectBtn = function($event) {
-            if(Number(ct.objectStorageQuator.teamQuator) == 0) {
-                common.showAlert("message","팀 할당량이 없어 오브젝트 스토리지를 생성 할 수 없습니다.");
-                return;
-            } else {
-                ct.fn.createStorageObject($event);
-            }
-        };
-
-        /*[생성(+)] 팝업 오픈*/
-        $scope.actionLoading = false; // action loading
-        $scope.authenticating = false; // action loading massage contents
-        $scope.actionBtnHied = false; // btn enabled
-        ct.fn.createStorageObject = function ($event) {
-            $scope.dialogOptions = {
-                controller : "iaasObjectStorageFormCtrl",
-                callBackFunction : ct.fn.getObjectStorageList
+            var dialogOptions =  {
+                controller       : "iaasObjectStorageFormCtrl" ,
+                callBackFunction : ct.objectStorageCallBackFunction
             };
+
             $scope.actionBtnHied = false;
-            common.showRightDialog($scope, $scope.dialogOptions);
+            common.showDialog($scope, $event, dialogOptions);
             $scope.actionLoading = true; // action loading
         };
 
-        if(ct.data.tenantId) {
+        ct.objectStorageCallBackFunction = function () {
+            ct.fn.getObjectStorageList();
+        };
+
+
+
+        if (ct.data.tenantId) {
             ct.fn.getObjectStorageList();
             ct.fn.getSendSecretInfoList();
+            ct.presentDate();
         }
     })
     //오브젝트스토리지 생성 컨트롤
     .controller('iaasObjectStorageFormCtrl', function ($scope, $location, $state,$translate,$timeout, $stateParams, $mdDialog, user, common, ValidationService, CONSTANTS ) {
         _DebugConsoleLog("objectStorageControllers.js : iaasObjectStorageFormCtrl", 1);
+        $scope.contents = common.getMainContentsCtrlScope().contents;
+        $scope.dialogOptions = common.getMainContentsCtrlScope().dialogOptions;
 
         var pop = this;
-        $scope.actionLoading = false;
+        pop.validationService 			= new ValidationService({controllerAs: pop});
+        pop.formName 					= $scope.dialogOptions.formName;
+        pop.userTenant 					= angular.copy($scope.main.userTenant);
+        pop.fn 							= {};
+        pop.data						= {};
+        pop.callBackFunction 			= $scope.dialogOptions.callBackFunction;
 
-        pop.userTenant = angular.copy($scope.main.userTenant);
-        pop.fn = {};
-        pop.data = {};
-        pop.formName = "objectStorageForm";
-        $scope.dialogOptions.formName = pop.formName;
-        $scope.dialogOptions.validDisabled = true;
-        $scope.dialogOptions.dialogClassName = "modal-lg";
-        $scope.dialogOptions.title = "오브젝트 스토리지 생성";
+        $scope.dialogOptions.title      = "저장소 생성";
+        $scope.dialogOptions.okName 	= "생성";
+        $scope.dialogOptions.closeName 	= "닫기";
         $scope.dialogOptions.templateUrl = _IAAS_VIEWS_ + "/storage/popObjectStorageForm.html" + _VersionTail();
-        $scope.dialogOptions.okName = $translate.instant("label.confirm");
+
+        $scope.actionLoading 			= false;
+        pop.btnClickCheck 				= false;
+        pop.validDisabled 				= true;
 
         // Dialog ok 버튼 클릭 시 액션 정의
-        $scope.dialogOptions.popDialogOk = function () {
-            pop.createObjectStorageAction();
+        $scope.popDialogOk = function () {
+            if ($scope.actionBtnHied) return;
+            $scope.actionBtnHied = true;
+
+            if (!pop.validationService.checkFormValidity(pop[pop.formName]))
+            {
+                $scope.actionBtnHied = false;
+                return;
+            }
+            pop.fn.createObjectStorageAction();
         };
 
-        $scope.popCancel = function () {
-            $mdDialog.hide();
+        $scope.popCancel = function() {
+            $scope.dialogClose = true;
+            common.mdDialogCancel();
         };
 
-        pop.createObjectStorageAction = function() {
+        pop.fn.createObjectStorageAction = function() {
+            $scope.main.loadingMainBody = true;
             pop.data.tenantId = pop.userTenant.tenantId;
             var params = {
                 tenantId : pop.data.tenantId,
@@ -153,6 +164,7 @@ angular.module('iaas.controllers')
                 $scope.dialogOptions.callBackFunction();
                 $scope.actionLoading = false;
                 $scope.actionBtnHied = false;
+                common.showAlertSuccess("저장소가 생성되었습니다.");
                 $mdDialog.hide();
             });
             returnPromise.error(function (data, status, headers) {
@@ -162,57 +174,3 @@ angular.module('iaas.controllers')
         };
     })
 
-  //오브젝트스토리지 접근 정보 컨트롤러
-    .controller('iaasAccessInfoFormCtrl', function ($scope, $location, $state,$translate,$timeout, $stateParams, user, common, ValidationService, CONSTANTS ) {
-        _DebugConsoleLog("objectStorageControllers.js : iaasAccessInfoFormCtrl", 1);
-
-        var pop = this;
-        $scope.actionLoading = false;
-
-        pop.userTenant = angular.copy($scope.main.userTenant);
-
-        pop.fn = {};
-        pop.data = {};
-
-        pop.formName = "objectStorageForm";
-        $scope.dialogOptions.formName = pop.formName;
-        $scope.dialogOptions.validDisabled = true;
-        $scope.dialogOptions.dialogClassName = "modal-lg";
-        $scope.dialogOptions.title = "오브젝트 스토리지 접근 정보";
-
-        $scope.dialogOptions.templateUrl = _IAAS_VIEWS_ + "/storage/popAccessInfoForm.html" + _VersionTail();
-        $scope.dialogOptions.okName = $translate.instant("label.confirm");
-
-        // Dialog ok 버튼 클릭 시 액션 정의
-        $scope.popDialogOk = function () {
-            if ($scope.actionBtnHied) return;
-            $scope.actionBtnHied = true;
-            $scope.popCancel();
-        };
-
-        $scope.popCancel = function () {
-            $scope.popHide();
-        };
-        /*내용 클릭 시 내용 copy*/
-        pop.fn.selectInput = function($event) {
-            $(event.target).select();
-            document.execCommand('copy');
-        };
-
-        pop.fn.sendSecretInfo = function() {
-            $scope.main.loadingMainBody = true;
-            var returnPromise = common.resourcePromise(CONSTANTS.iaasApiContextUrl + '/storage/objectStorage/sendSecretInfo', 'GET', {tenantId:pop.userTenant.tenantId},'application/x-www-form-urlencoded');
-            returnPromise.success(function (data, status, headers) {
-                pop.secret = data.content.secret;
-            });
-            returnPromise.error(function (data, status, headers) {
-                common.showAlert("message",data.message);
-                $scope.main.loadingMainBody = false;
-            });
-            returnPromise.finally(function (data, status, headers) {
-                $scope.main.loadingMainBody = false;
-            });
-        };
-
-        pop.fn.sendSecretInfo();
-    });
