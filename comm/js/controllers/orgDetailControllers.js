912f8640f5afe005e454b7b84b0605ca4f5440c2
mhwalee <mhwalee@crossent.com>
2019-08-09 17:57:27 +0900

    cpu 챠트 순서 변경

--- comm/js/controllers/orgDetailControllers.js
+++ comm/js/controllers/orgDetailControllers.js
@@ -997,11 +997,11 @@ angular.module('portal.controllers')
 
             // IaaS 정보
             if ($scope.main.userTenantId) {
+                ct.getIaasInseanceInfos($scope.main.userTenantId);
+                ct.getIaasResourceUsed($scope.main.userTenantId);
                 $timeout (function () {
                     ct.iaasCpuStatusChart();
                 }, 500);
-                ct.getIaasInseanceInfos($scope.main.userTenantId);
-                ct.getIaasResourceUsed($scope.main.userTenantId);
             }
 
             // PaaS 정보
