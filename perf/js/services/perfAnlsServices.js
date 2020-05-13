79397a74c1cdd1dee9757319772ea1bb9bc42f01
Shin-JungYeon <ssjy11@gmail.com>
2020-05-13 14:40:18 +0900

    화면 ID 및 기능 주석 추가

--- perf/js/services/perfAnlsServices.js
+++ perf/js/services/perfAnlsServices.js
@@ -5,27 +5,27 @@ angular.module('perf.services')
 
         var perfAnlsService = {};
 
-        /* 월별 과금 리스트 BY ORGCODE */
+        /* 월별 과금 현황 BY ORGCODE - 화면 ID: HUBPOP_INT_PER_ANS_02  */
         perfAnlsService.totalAnlsByOrgCodeAndPerfYm = function (params) {
             return common.retrieveResource(common.resourcePromise(CONSTANTS.uaaContextUrl + '/perf/anls/org/{orgCode}/monthly/total', 'GET', params));
         };
 
-        /* 프로젝트별 과금 모든 금액 통합 월별 아이템그룹별 조회 */
+        /* 대시보드 월별/아이템그룹별 과금 합계 BY ORGCODE - 화면 ID: HUBPOP_INT_PER_ANS_01 */
         perfAnlsService.listAnlsTotalByOrgAndItemGroup = function (params) {
             return common.retrieveResource(common.resourcePromise(CONSTANTS.uaaContextUrl + '/perf/anls/org/{orgCode}/itemgroup/total', 'GET', params));
         }
 
-        /* 프로젝트별 과금 모든 금액 통합 월별 아이템별 조회 */
+        /* 대시보드 월별/아이템별 과금 합계 BY ORGCODE - 화면 ID: HUBPOP_INT_PER_ANS_01 */
         perfAnlsService.listAnlsTotalByOrgAndItem = function (params) {
             return common.retrieveResource(common.resourcePromise(CONSTANTS.uaaContextUrl + '/perf/anls/org/{orgCode}/item/total', 'GET', params));
         }
 
-        /* 프로젝트별 과금 통합 월별 추이 조회 */
+        /* 대시보드 월별 과금 추이 BY ORGCODE - 화면 ID: HUBPOP_INT_PER_ANS_01 */
         perfAnlsService.listAnlsMonthlySummaryByOrg = function (params) {
             return common.retrieveResource(common.resourcePromise(CONSTANTS.uaaContextUrl + '/perf/anls/org/{orgCode}/summary', 'GET', params));
         }
 
-        /* 프로젝트별 과금 통합 일별 추이 조회 */
+        /* 대시보드 일별 과금 추이 BY ORGCODE - 화면 ID: HUBPOP_INT_PER_ANS_01 */
         perfAnlsService.listAnlsDailySummaryByOrg = function (params) {
             return common.retrieveResource(common.resourcePromise(CONSTANTS.uaaContextUrl + '/perf/anls/org/{orgCode}/daily/summary', 'GET', params));
         }
