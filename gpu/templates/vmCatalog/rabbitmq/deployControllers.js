2ddcc488a55a762190af2d30dc2c99312dbb856e
Shin-JungYeon <ssjy11@gmail.com>
2020-06-23 15:47:45 +0900

    VM 서비스 카탈로그 rabbitmq 수정

--- gpu/templates/vmCatalog/rabbitmq/deployControllers.js
+++ gpu/templates/vmCatalog/rabbitmq/deployControllers.js
@@ -13,21 +13,23 @@ angular.module('gpu.controllers')
 
         ct.data.servicePort = 5672;
         ct.data.managementPort = 15672;
-        ct.data.lbSvcPort = 5672;
-        ct.data.checkPort = 9898;
+        ct.data.epmdPort = 4369;
+        ct.data.erlangPort = 25672;
+        ct.data.erlangCookie = "CMAXAFWPGKUBELOPUZOP";
 
         // 테스트 입력값
         ct.data.deployName = "래빗엠큐";
-        ct.data.stackName = "Rabbitmq";
 
+        ct.data.stackName = "Rabbitmq";
         ct.data.adminPassword = "Crossent!234";
         ct.data.adminConfirmPassword = "Crossent!234";
-        ct.data.erlangCookie = "CMAXAFWPGKUBELOPUZOP";
 
         // 추가 셋팅
         subPage.fn.appendSetVmCatalogDeploy = function (vmCatalogDeploy) {
             vmCatalogDeploy.parameters.amqp_port = ct.data.servicePort;
             vmCatalogDeploy.parameters.management_port = ct.data.managementPort;
+            vmCatalogDeploy.parameters.epmd_port = ct.data.epmdPort;
+            vmCatalogDeploy.parameters.erlang_port = ct.data.erlangPort;
             vmCatalogDeploy.parameters.admin_password = ct.data.adminPassword;
             if(ct.data.deployType == "cluster") {
                 vmCatalogDeploy.parameters.erlang_cookie = ct.data.erlangCookie;
