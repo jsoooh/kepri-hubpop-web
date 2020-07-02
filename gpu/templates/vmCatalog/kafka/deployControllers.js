3f828dcf7708fd5c917c350db65462accb54f89c
Shin-JungYeon <ssjy11@gmail.com>
2020-07-02 14:24:05 +0900

    VM 서비스 카탈로그 kafka 수정

--- gpu/templates/vmCatalog/kafka/deployControllers.js
+++ gpu/templates/vmCatalog/kafka/deployControllers.js
@@ -8,19 +8,22 @@ angular.module('gpu.controllers')
         var ct = $scope.$parent.$parent.contents;
         ct.vs = new ValidationService({controllerAs: $scope.subPage});
 
+        ct.data.servicePort = 9092;
+        ct.data.zookeeperPort = 2181;
+
+        ct.usingPorts.cluster = ['2888', '3888'];
+
+        ct.data.deployType = "single";
+
         // single 테스트
         if (ct.testInput) {
             ct.data.deployName = "DB 카프카";
             ct.data.stackName = "kafka";
-            ct.data.deployType = "single";
-            ct.data.servicePort = 9092;
-            ct.data.zookeeperPort = 2181;
             /*
             ct.data.deployType = "cluster";
              */
         }
 
-        ct.usingPorts.cluster = ['2888', '3888'];
         ct.fn.kafkaPortCustomValidationCheck = function(port, type) {
             if (port == undefined || port == null || port == "") return;
             if (type == "kafka" && port == ct.data.zookeeperPort) {
@@ -30,7 +33,8 @@ angular.module('gpu.controllers')
             } else {
                 return {isValid: true};
             }
-        }
+        };
+
         // 추가 셋팅
         subPage.fn.appendSetVmCatalogDeploy = function (vmCatalogDeploy) {
             vmCatalogDeploy.parameters.service_port = ct.data.servicePort;
