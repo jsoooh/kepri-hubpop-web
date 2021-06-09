9264efa1cd3658e607b42695ee87dd3aec32acd7
mhyukkim <kimbob9108@naver.com>
2021-06-09 15:52:20 +0900

    GPU 인스턴스 만들기 인스턴스 유형이 GPU 라면 OS 타입은 ubuntu 18.04만 보이게 할것

--- gpu/js/controllers/compute/computeControllers.js
+++ gpu/js/controllers/compute/computeControllers.js
@@ -1489,11 +1489,13 @@ angular.module('gpu.controllers')
         ct.fn.imageTypeListSearch = function() {
             $scope.main.loadingMainBody = true;
 
+            ct.imageTypeFilterList = [];
             ct.imageTypeList  = [];
             var returnPromise = common.resourcePromise(CONSTANTS.gpuApiContextUrl + '/server/image/osType', 'GET');
             returnPromise.success(function (data, status, headers) {
                 if (data && data.content) {
                     ct.imageTypeList = data.content;
+                    ct.imageTypeFilterList = angular.copy(data.content);
                 }
             });
             returnPromise.error(function (data, status, headers) {
@@ -1591,6 +1593,18 @@ angular.module('gpu.controllers')
         ct.fn.specTypeChange = function () {
             ct.sltImageId = '';
             ct.fn.getAvailabilityZoneList();
+
+            // OS 타입 필터링 (유형이 GPU 인건은 ubuntu 18.04 건만 보이게(이정일 차장 요구) 2021.06.09)
+            if (ct.selectedSpecType == 'GPU') {
+                ct.imageTypeFilterList = [];
+                angular.forEach(ct.imageTypeList, function (imageType) {
+                   if (imageType.osType == 'ubuntu' && imageType.imageVersion == '18.04') {
+                       ct.imageTypeFilterList.push(imageType);
+                   }
+                });
+            } else {
+                ct.imageTypeFilterList = angular.copy(ct.imageTypeList);
+            }
         };
 
         ct.fn.getGpuCardList = function() {
